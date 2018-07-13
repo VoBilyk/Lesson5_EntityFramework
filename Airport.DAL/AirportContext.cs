d1ff6948730cb0da495885a37aafff84398f4b6a
Volodymyr <34250821+VoBilyk@users.noreply.github.com>
2018-07-14 00:26:12 +0300

    added EF

---
+++ Airport.DAL/AirportContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Airport.DAL.Models;
+
+
+namespace Airport.DAL
+{
+    public class AirportContext : DbContext
+    {
+        public DbSet<Ticket> Tickets { get; set; }
+
+
+        public AirportContext() : base()
+        {
+
+        }
+    }
+}
